79887c5f0b2219224151ba7f354bf3832a2b6135
Görkem <gorkemyelken@gmail.com>
2021-11-13 22:58:43 +0300

    Implement My Dictionary

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+
+            myDictionary.Add("Red", "Kırmızı");
+            myDictionary.Add("Yellow", "Sarı");
+
+            myDictionary.Print();
+        }
+    }
+}
