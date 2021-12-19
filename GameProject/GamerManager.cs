5f55e722b5d848ec4abc404727a53125c24c263b
Görkem <gorkemyelken@gmail.com>
2021-12-19 19:11:55 +0300

    Game Project

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class GamerManager : IGamerService
+    {
+        public void Add(User user)
+        {
+            Console.WriteLine("Kayıt oldu");
+        }
+
+        public void Delete(User user)
+        {
+            Console.WriteLine("Kayıt silindi");
+        }
+
+        public void Update(User user)
+        {
+            Console.WriteLine("Kayıt güncellendi");
+        }
+    }
+}
---
+++ GameProject/IGamerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    interface IGamerService
+    {
+        void Add(User user);
+        void Update(User user);
+        void Delete(User user);
+    }
+}
